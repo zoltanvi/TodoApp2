0309ea892309ae9f692ac068f3405762922feea4
Zoltan <viczezoltan@gmail.com>
2022-11-19 21:14:43 +0100

    Implemented word wrap settings for Note page

---
+++ TodoApp2/ValueConverters/Single/FromBool/BoolToScrollBarVisibilityConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace TodoApp2
+{
+    internal class BoolToScrollBarVisibilityConverter : BaseBoolValueConverter<ScrollBarVisibility>
+    {
+        // Wrap
+        protected override ScrollBarVisibility PositiveValue => ScrollBarVisibility.Disabled;
+        
+        // No wrap
+        protected override ScrollBarVisibility NegativeValue => ScrollBarVisibility.Hidden;
+    }
+}
