79e38ed33355bc21cd30e5ce4b63e1997babf17e
Zoltan <viczezoltan@gmail.com>
2022-12-08 21:39:34 +0100

    Optimized an SQL query

--- TodoApp2.Core/Database/DataAccessLayer.cs
+++ TodoApp2.Core/Database/DataAccessLayer.cs
@@ -372,6 +372,30 @@ namespace TodoApp2.Core
         }
 
         /// <summary>
+        /// Returns the number of categories in the database
+        /// </summary>
+        /// <returns></returns>
+        private int GetCategoryCount()
+        {
+            int count = 0;
+
+            using (SQLiteCommand command = new SQLiteCommand(m_Connection))
+            {
+                command.CommandText = $"SELECT COUNT(*) FROM {Column.Category} ";
+
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        count = reader.GetInt32(0);
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
         /// Gets the category record with the provided name from the Category table
         /// </summary>
         /// <param name="name"></param>
@@ -991,7 +1015,7 @@ namespace TodoApp2.Core
         private void AddDefaultCategoryIfNotExists()
         {
             // This should only happen when the application database is just created
-            if (GetCategories().Count == 0)
+            if (GetCategoryCount() == 0)
             {
                 AddCategory(new CategoryListItemViewModel
                 {
@@ -1003,7 +1027,7 @@ namespace TodoApp2.Core
                 AddCategory(new CategoryListItemViewModel
                 {
                     Id = 1,
-                    Name = "Shortcuts",
+                    Name = "Help",
                     ListOrder = DefaultListOrder + 1
                 });
 
