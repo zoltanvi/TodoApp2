87da252426f544f100375c6f7c5a5e8b1c25c611
Zoltan <viczezoltan@gmail.com>
2023-05-21 22:28:04 +0200

    Fix initial click sound when the app is opened

--- TodoApp2.Core/Services/MediaPlayerService.cs
+++ TodoApp2.Core/Services/MediaPlayerService.cs
@@ -11,9 +11,12 @@ namespace TodoApp2.Core
         public MediaPlayerService()
         {
             m_MediaPlayer = new MediaPlayer();
+            double volume = m_MediaPlayer.Volume;
+            m_MediaPlayer.Volume = 0;
             m_MediaPlayer.Open(new Uri("Sounds/click.mp3", UriKind.Relative));
             m_MediaPlayerReverse = new MediaPlayer();
             m_MediaPlayerReverse.Open(new Uri("Sounds/click_reverse.mp3", UriKind.Relative));
+            m_MediaPlayer.Volume = volume;
         }
 
         public void PlayClick()
