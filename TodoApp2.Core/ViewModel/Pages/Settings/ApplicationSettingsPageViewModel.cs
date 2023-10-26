97c35caacac1bb84ecb4020601b6cc956f88cd80
Zoltan <viczezoltan@gmail.com>
2023-10-26 20:03:29 +0200

    Add button to open database location in explorer

---
+++ TodoApp2.Core/ViewModel/Pages/Settings/ApplicationSettingsPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace TodoApp2.Core
+{
+    public class ApplicationSettingsPageViewModel : BaseViewModel
+    {
+        public ICommand OpenDbLocationCommand { get; set; }
+
+        public ApplicationSettingsPageViewModel()
+        {
+            OpenDbLocationCommand = new RelayCommand(OpenDbLocation);
+        }
+
+        private void OpenDbLocation()
+        {
+            try
+            {
+                Process.Start("explorer.exe", $@"/select,""{IoC.AppViewModel.DatabaseLocation}""");
+            }
+            catch (Exception ex)
+            {
+                // Ignore for the time being
+            }
+        }
+    }
+}
--- TodoApp2.Core/ViewModel/Pages/Settings/WindowSettingsPageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TodoApp2.Core
-{
-    public class ApplicationSettingsPageViewModel : BaseViewModel
-    {
-        public ApplicationSettingsPageViewModel()
-        {
-        }
-    }
-}
