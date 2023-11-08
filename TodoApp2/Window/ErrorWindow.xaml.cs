f4704c6ff0a403c1401b78009d0a5d65e0d30979
Zoltan <viczezoltan@gmail.com>
2023-11-08 13:21:38 +0100

    Add error window to display exceptions

---
+++ TodoApp2/Window/ErrorWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using TodoApp2.Core;
+
+namespace TodoApp2
+{
+    /// <summary>
+    /// Interaction logic for ErrorWindow.xaml
+    /// </summary>
+    public partial class ErrorWindow : Window
+    {
+        private ErrorWindowViewModel _viewModel;
+
+        public ErrorWindow(string title, string errorMessage)
+        {
+            InitializeComponent();
+            _viewModel = new ErrorWindowViewModel(title, errorMessage);
+            DataContext = _viewModel; 
+        }
+
+        public void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public class ErrorWindowViewModel : BaseViewModel
+    {
+        public ErrorWindowViewModel(string title, string errorMessage)
+        {
+            Title = title;
+            ErrorMessage = errorMessage;
+        }
+
+        public string Title { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
